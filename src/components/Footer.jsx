b37ff8ce8242678e122613d9f867552288f94d74
Vyrabot <102457168+Vyrabot@users.noreply.github.com>
2025-03-02 11:51:33 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="bg-servicesBg h-[80px] flex justify-center items-center">
       <p className="text-sm md:font-xl dark:text-white">
-        &copy; 2024 Chamreun Vira. All Rights Reserved
+        &copy; 2025 Chamreun Vira. All Rights Reserved
       </p>
     </div>
   );
